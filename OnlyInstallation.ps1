beed1c822c851eafe93fae80803da4ff71b9d443
cloudunicorn-cupl <vignesh@cloudunicorn.in>
2024-05-20 13:56:16 +0530

    OnlyInstallation.ps1

--- OnlyInstallation.ps1
+++ OnlyInstallation.ps1
@@ -2,7 +2,7 @@
 $redirectLink = "https://in01-cloud.acronis.com/bc/api/ams/links/agents/redirect?language=multi&channel=CURRENT&system=windows&productType=enterprise&login=9adbc0a7-598a-4d75-a861-a5ee60a168d0&white_labeled=0"
 
 # Set the token for registration
-$registrationToken = "DBEF-D2A9-4891"
+$registrationToken = "33A7-EEE6-4E2D"
 
 # Function to download the installer from the redirecting link
 function Download-Installer {
