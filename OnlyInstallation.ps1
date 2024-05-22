b3262d906404504d6c52bc5cff70b5d719c79c55
cloudunicorn-cupl <vignesh@cloudunicorn.in>
2024-05-22 10:03:09 +0530

    OnlyInstallation.ps1

--- OnlyInstallation.ps1
+++ OnlyInstallation.ps1
@@ -1,31 +1,29 @@
 # Set the redirecting link
-$A = "https://in01-cloud.acronis.com/bc/api/ams/links/agents/redirect?language=multi&channel=CURRENT&system=windows&productType=enterprise&login=9adbc0a7-598a-4d75-a861-a5ee60a168d0&white_labeled=0"
+$redirectLink = "https://in01-cloud.acronis.com/bc/api/ams/links/agents/redirect?language=multi&channel=CURRENT&system=windows&productType=enterprise&login=9adbc0a7-598a-4d75-a861-a5ee60a168d0&white_labeled=0"
 
-# Set the token for registration
-$B = "506D-90BD-403D"
 
 # Function to download the installer from the redirecting link
-function C {
+function Download-Installer {
     param (
-        [string]$D,
-        [string]$E
+        [string]$Link,
+        [string]$Directory
     )
 
     # Send a web request to follow the redirect
-    $F = Invoke-WebRequest -Uri $D -MaximumRedirection 0 -ErrorAction SilentlyContinue -UseBasicParsing
+    $response = Invoke-WebRequest -Uri $Link -MaximumRedirection 0 -ErrorAction SilentlyContinue
 
     # Check if the response contains a redirect
-    if ($F.StatusCode -eq 302 -and $F.Headers.Location) {
+    if ($response.StatusCode -eq 302 -and $response.Headers.Location) {
         # Extract the actual download link from the redirect response
-        $G = $F.Headers.Location
+        $downloadLink = $response.Headers.Location
 
         # Set the path where you want to save the installer
-        $H = Join-Path -Path $E -ChildPath (Split-Path $G -Leaf)
+        $installerPath = Join-Path -Path $Directory -ChildPath (Split-Path $downloadLink -Leaf)
 
         # Download the installer
-        Invoke-WebRequest -Uri $G -OutFile $H
+        Invoke-WebRequest -Uri $downloadLink -OutFile $installerPath
 
-        return $H
+        return $installerPath
     } else {
         Write-Host "Error: The redirect link did not lead to a download link."
         return $null
@@ -33,18 +31,17 @@ function C {
 }
 
 # Set the directory where you want to save the installer
-$I = "C:\Path\To\Save\Installer"
+$installerDirectory = "C:\Acronis"
 
 # Ensure that the directory exists
-if (-not (Test-Path $I)) {
-    New-Item -Path $I -ItemType Directory -Force
+if (-not (Test-Path $installerDirectory)) {
+    New-Item -Path $installerDirectory -ItemType Directory -Force
 }
 
 # Download the installer
-$J = C -D $A -E $I
+$installerPath = Download-Installer -Link $redirectLink -Directory $installerDirectory
 
-if ($J) {
+if ($installerPath) {
     # Run the installer with the specified token
-    $K = "--registration by-token --reg-token $B --reg-address $J"
-    Start-Process -FilePath $J -ArgumentList $K -Wait
+    Start-Process -FilePath $installerPath -ArgumentList "--reg-address=https://in01-cloud.acronis.com --registration=by-token --reg-token=506D-90BD-403D" -Wait
 }
